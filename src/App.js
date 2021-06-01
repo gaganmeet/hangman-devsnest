b1f20d523753f043f75b4d3d8229e2d114a19478
Gaganmeet Bahri <gaganmeet.bahri96@gmail.com>
2021-06-01 19:35:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,111 @@ import axios from "axios";
 
 function App() {
   const baseUrl = `https://random-word-api.herokuapp.com/word?number=1`;
-  const [apiUrl, setApiUrl] = useState(baseUrl);
+  const alphabets = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
   const [word, setWord] = useState("");
-  useEffect(
-    () => axios.get(apiUrl).then((apidata) => setWord(apidata.data)),
-    [apiUrl]
-  );
-  console.log("word", word);
-  console.log("api url", apiUrl);
+  const [guessList, setGuessList] = useState([]);
+  const [guesses, setGuesses] = useState(7);
+  const [game, setGame] = useState(true);
+  const getWord = (word) =>
+    axios
+      .get(baseUrl)
+      .then((apidata) => {
+        setWord(apidata.data);
+        return apidata.data;
+      })
+      .then((data) => makeEmpty(data));
+  useEffect(() => {
+    getWord();
+  }, []);
+
+  const makeEmpty = (word) => {
+    let temp = [];
+    for (let i = 0; i < word[0].length; i++) {
+      temp.push("_");
+    }
+    setGuessList(temp);
+  };
+  const alphabetClick = (idx) => {
+    if (guesses === 0) {
+      setGame((prev) => (prev = false));
+      return;
+    }
+    let alphabet = alphabets[idx];
+    let w = word[0].toUpperCase();
+    w = w.split("");
+    let found = w.includes(alphabet);
+    let temp = guessList;
+    if (found !== false) {
+      for (let i = 0; i < w.length; i++) {
+        if (w[i] === alphabet) temp[i] = alphabet;
+      }
+      setGuessList([...temp]);
+    } else {
+      setGuesses((prev) => prev - 1);
+    }
+  };
+  const playAgain = () => {
+    setGame(true);
+    setGuesses(7);
+    getWord();
+  };
   return (
     <div className="App">
-      <h1>{word[0]}</h1>
-      <button onClick={() => setApiUrl(baseUrl + "")}>Change word</button>
+      <h1>Hangman Game</h1>
+      {game ? (
+        <>
+          <div className="guesslist">
+            {guessList.map((item, idx) => (
+              <h1 key={idx}>{item}</h1>
+            ))}
+          </div>
+          <div className="guessBtn">
+            {alphabets.map((item, idx) => (
+              <button
+                key={idx}
+                onClick={() => alphabetClick(idx)}
+                className="alphBtn"
+              >
+                {item}
+              </button>
+            ))}
+          </div>
+          <button onClick={() => getWord()}>Change word</button>
+          <h1>Guesses left : {guesses}</h1>
+        </>
+      ) : (
+        <>
+          <h1>Game Over</h1>
+          <button onClick={() => playAgain()}>Play Again</button>
+        </>
+      )}
     </div>
   );
 }
